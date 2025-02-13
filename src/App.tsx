2dff320ed81bb4acacecf597ba4ce787ca2f75d1
Abachouch Youssouf <youssouf.abachouch@gmail.com>
2025-02-13 14:52:58 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { ColorsLab } from "./Colorslab";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1>Color wizard</h1>
+        <ColorsLab></ColorsLab>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Colorslab.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { getColors, _variants } from "theme-colors";
+import { isHex } from "is-color";
+
+import chroma from "chroma-js";
+
+export const ColorsLab = () => {
+  const [hex, setHex] = useState("#3498db"); // Default color
+  const [hsl, setHsl] = useState<[number, number, number]>();
+  const [tints, setTints] = useState<string[]>([]);
+  const [shades, setShades] = useState<string[]>([]);
+  const [themeClrs, setThemeClrs] = useState<{}>({});
+
+  useEffect(() => {
+    if (isHex(hex)) {
+      if (hex.length === 4 || hex.length === 7) {
+        setHsl(chroma(hex).hsl());
+        const shades2 = getTheme(hex);
+        const { tints: t, shades: s } = generateShadesTints(hex);
+        setTints(t);
+        setShades(s);
+        setThemeClrs(shades2);
+      }
+    }
+  }, [hex]);
+
+  const getTheme = (color: string) => {
+    const colors = getColors(color);
+    console.log("colors", colors);
+    return colors;
+  };
+
+  function generateShadesTints(color: string) {
+    let base = chroma(color);
+    let tints = Array.from({ length: 5 }, (_, i) => base.brighten(i + 1).hex());
+    let shades = Array.from({ length: 5 }, (_, i) => base.darken(i + 1).hex());
+    return { tints, shades };
+  }
+
+  return (
+    <div>
+      <header className="header">
+        <input
+          type="text"
+          value={hex}
+          onChange={(e) => setHex(e.target.value)}
+        />
+        <div className="selectedColor" style={{ backgroundColor: hex }}></div>
+      </header>
+
+      <h3>shades</h3>
+      <div className="results-section">
+        <div className="shades">
+          {shades.map((shade, i) => (
+            <div key={i} className="shade" style={{ backgroundColor: shade }}>
+              {shade}
+            </div>
+          ))}
+        </div>
+
+        <h3>tints</h3>
+        <div className="shades">
+          {tints.map((shade, i) => (
+            <div key={i} className="shade" style={{ backgroundColor: shade }}>
+              {shade}
+            </div>
+          ))}
+        </div>
+
+        <h3>theme colors</h3>
+        <div className="shades">
+          {Object.entries(themeClrs).map(([shade, color], i) => (
+            <div
+              key={i}
+              className="shade"
+              style={{ backgroundColor: color as string }}
+            >
+              {shade} - {color as string}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
